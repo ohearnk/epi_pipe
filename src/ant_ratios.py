7d637fd1cff3776d29856ac2aefe13e5c9df49f0
ohearnk <ohearnk@okami.cis.gvsu.edu>
2013-04-10 12:52:36 -0400

    Modularized the code, and began work on reproducing similar plots for call time.

--- src/ant_ratios.py
+++ src/ant_ratios.py
@@ -2,15 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import preprocessing as pp
 
-ant_ratios = []
-cum_ratios = []
-FILES = [ '/opt2/D4D/data/SET1/adj_mats/cum_num_calls/set1_t'+str(i)+'_cum_num_calls.npy'
-    for i in xrange(10) ]
 
-# process all of the adjacency matrices
-for FILE in FILES:
-    adj_mat = pp.load_bin_adj_mat(FILE)
-
+def inter_indiv_call_ratios(adj_mat, t, indiv_call_ratios, 
+        cum_call_ratios, num_no_calls):
+    # calculate the ratios for all antennas
     indiv_ratios = []
     for i in xrange(adj_mat.shape[0]):
         row_sum = np.sum(adj_mat[i,:])
@@ -19,7 +14,8 @@ for FILE in FILES:
         # tempoary fix to avoid division by zero
         else:
             indiv_ratios.append(0.0)
-    ant_ratios.append(indiv_ratios)
+            num_no_calls[t] += 1
+    indiv_call_ratios.append(indiv_ratios)
 
     # calculate cumulative ratios
     num_inner_calls = np.trace(adj_mat)
@@ -28,24 +24,44 @@ for FILE in FILES:
         for j in xrange(adj_mat.shape[1]):
             if i < j:
                 num_outer_calls = num_outer_calls + adj_mat[i,j]
-    cum_ratios.append(float(num_inner_calls) / (num_inner_calls+num_outer_calls))
-
-#print "Individual ratios:"
-#print ant_ratios
-#print
-print "Cumulative ratios:"
-for i in xrange(len(cum_ratios)):
-    print "\tTime period %d: %f" % (i, cum_ratios[i])
-
-for i in xrange(10):
-#    plt.plot([j for j in xrange(len(ant_ratios[i]))], ant_ratios[i])
-#    plt.title("Inter-Antenna Ratios by Antenna, Time Period "+str(i))
-#    plt.xlabel("Antenna ID")
-#    plt.ylabel("Ratio of Inter-Antenna Calls to All Calls")
-    ant_ratios[i].sort()
-    ant_ratios[i].reverse()
-    plt.plot([j for j in xrange(len(ant_ratios[i]))], ant_ratios[i])
-    plt.title("Inter-Antenna Ratios by Decreasing Ratio, Time Period "+str(i))
-    plt.xlabel("Meaningless Number")
+    cum_call_ratios.append(float(num_inner_calls) / (num_inner_calls+num_outer_calls))
+    
+    # plot the sorted individual call data
+    # note: use the local indiv_ratios list instead of the multi-time period indiv_call_ratios
+    # list since this plot required the data be manipulated in-place
+    indiv_ratios.sort()
+    indiv_ratios.reverse()
+    plt.plot([j for j in xrange(len(indiv_ratios))], indiv_ratios)
+    plt.title("Inter-Antenna Ratios In Decreasing Order, Time Period %d" % t)
+    plt.xlabel("Antenna IDs (Scrambled After Sorting)")
     plt.ylabel("Ratio of Inter-Antenna Calls to All Calls")
-    plt.show()
+    plt.savefig("inter_antenna_sorted_t%d.pdf" % t)
+    # clear the state of the current figure and release memory
+    plt.close()
+
+    return indiv_call_ratios, cum_call_ratios, num_no_calls
+
+
+if __name__ == "__main__":
+    indiv_call_ratios = []
+    indiv_time_ratios = []
+    cum_call_ratios = []
+    cum_time_ratios = []
+    CALL_FILES = [ '/opt2/D4D/data/SET1/adj_mats/cum_num_calls/set1_t%d_cum_num_calls.npy' % i
+        for i in xrange(10) ]
+    TIME_FILES = [ '/opt2/D4D/data/SET1/adj_mats/cum_num_calls/set1_t%d_cum_call_time.npy' % i
+        for i in xrange(10) ]
+    num_no_calls = [ 0 for i in xrange(10) ]
+
+    # process all of the adjacency matrices
+    for i, FILE in enumerate(CALL_FILES):
+        adj_mat = pp.load_bin_adj_mat(FILE)
+
+        # calculate and plot the call ratios for this time period
+        indiv_call_ratios, cum_call_ratios, num_no_calls = \
+                inter_indiv_call_ratios(adj_mat, i, indiv_call_ratios, cum_call_ratios, num_no_calls)
+
+    print "Cumulative ratios:"
+    for i in xrange(len(cum_call_ratios)):
+        print "\tTime period %d: %f" % (i, cum_call_ratios[i])
+        print "\t\tNumber of antennas without any calls: %d" % num_no_calls[i]
